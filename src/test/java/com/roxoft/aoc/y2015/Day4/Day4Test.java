f19f6aaf316c19acb8ddf233eb6dfe30fc7e0724
rossdrew <thek1ng13@hotmail.com>
2023-11-08 14:07:39 +0000

    [AoC 2015] Problem Statement B

--- src/test/java/com/roxoft/aoc/y2015/Day4/Day4Test.java
+++ src/test/java/com/roxoft/aoc/y2015/Day4/Day4Test.java
@@ -23,6 +23,9 @@ import static org.junit.jupiter.api.Assertions.*;
  * (000001dbbfa...), and it is the lowest such number to do so.
  * If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is
  * 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+ *
+ * --- Part Two ---
+ * Now find one that starts with six zeroes.
  */
 public class Day4Test {
     @Test
