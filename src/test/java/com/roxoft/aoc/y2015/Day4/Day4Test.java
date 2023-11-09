f2f7f83f1d1d88e2a63f3f381ee195d88e419880
rossdrew <thek1ng13@hotmail.com>
2023-11-09 12:44:10 +0000

    Remove redundant test

--- src/test/java/com/roxoft/aoc/y2015/Day4/Day4Test.java
+++ src/test/java/com/roxoft/aoc/y2015/Day4/Day4Test.java
@@ -13,18 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
  * --- Day 4: The Ideal Stocking Stuffer ---
  * Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the economically
  * forward-thinking little girls and boys.
- *
  * To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes. The input to the MD5
  * hash is some secret key (your puzzle input, given below) followed by a number in decimal. To mine AdventCoins, you
  * must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
- *
  * For example:
- *
  * If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes
  * (000001dbbfa...), and it is the lowest such number to do so.
  * If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is
  * 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
- *
  * --- Part Two ---
  * Now find one that starts with six zeroes.
  */
@@ -51,11 +47,6 @@ public class Day4Test {
         assertFalse(Analyse.string("12345").startsWith(5, '0'));
         assertFalse(Analyse.string("1234").startsWith(5, '0'));
     }
-
-//    @Test
-//    public void requirement_findNextFiveZeroMD5(){
-//        assertEquals("abcdef609043".toUpperCase(), Day4.for("a"));
-//    }
 
     @Test
     public void example_A1() throws NoSuchAlgorithmException {
