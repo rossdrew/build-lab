00ccdbadd414217be2731b463d4e94adee2f848c
rossdrew <thek1ng13@hotmail.com>
2023-11-10 12:11:28 +0000

    Checktyle fix

--- src/main/java/com/roxoft/lib/From.java
+++ src/main/java/com/roxoft/lib/From.java
@@ -33,15 +33,13 @@ public final class From {
      * @return An MD5 hashed {@link String} of the specified number of charactes in hexadecimal.
      * @throws NoSuchAlgorithmException if MD5 cannot be used for any reason
      */
-    public String toMD5Hex(int hexCharacters) throws NoSuchAlgorithmException {
-        if (hexCharacters > 16){
-            hexCharacters = 16;
-        }
+    public String toMD5Hex(final int hexCharacters) throws NoSuchAlgorithmException {
+        final int hexCharactersToGenerate = Math.min(hexCharacters, 16);
         final byte[] bytesOfMessage = value.getBytes(StandardCharsets.UTF_8);
         final MessageDigest md = MessageDigest.getInstance("MD5");
         final byte[] mD5digest = md.digest(bytesOfMessage);
         final StringBuilder hexStringBuilder = new StringBuilder();
-        for (int i = 0; i < hexCharacters; i++) {
+        for (int i = 0; i < hexCharactersToGenerate; i++) {
             hexStringBuilder.append(String.format("%02X", mD5digest[i]));
         }
         return hexStringBuilder.toString();
